c75ac20cfc52619970f3c263296faa50961670ac
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2025-02-27 20:34:57 +0300

    Remove languages that I won't support (in near future), update query for Go

--- src/tree-sitter/languages/index.ts
+++ src/tree-sitter/languages/index.ts
@@ -25,40 +25,30 @@ const Rust: Language = {
 };
 const Go: Language = {
     vscodeId: 'go',
-    function: [],
-    // new Map([
-    //     ['function_declaration', 'identifier'],
-    //     ['method_declaration', 'field_identifier'],
-    // ]),
+    function: [
+        new QueryItem(tag.fun, 'function_declaration', [
+            new QueryItem(tag.name, 'identifier'),
+            new QueryItem(tag.args, 'parameter_list'),
+            new QueryItem(tag.body, 'block'),
+        ]),
+        new QueryItem(tag.fun, 'method_declaration', [
+            new QueryItem(tag.name, 'field_identifier'),
+            new QueryItem(tag.args, 'parameter_list'),
+            new QueryItem(tag.body, 'block'),
+        ]),
+    ],
 };
 
-// ToDo: test (somehow)
-const Ruby: Language = {
-    vscodeId: 'ruby',
-    function: [],
-    // new Map([
-    //     // empty
-    // ]),
+const Java: Language = {
+    vscodeId: 'java',
+    function: [
+        new QueryItem(tag.fun, 'method_declaration', [
+            new QueryItem(tag.name, 'identifier'),
+            new QueryItem(tag.args, 'formal_parameters'),
+            new QueryItem(tag.body, 'block'),
+        ]),
+    ],
 };
-// ToDo: test (somehow)
-const Scala: Language = {
-    vscodeId: 'scala',
-    function: [],
-    // new Map([
-    //     // empty
-    // ]),
-};
-// ToDo: test (somehow)
-const Lua: Language = {
-    vscodeId: "lua",
-    function: [],
-    // new Map([
-    //     // 'function_definition'
-    //     // ['function_declaration', 'function_name'],
-    //     // ['local_function_declaration', 'identifier'],
-    // ]),
-};
-
 const CSharp: Language = {
     vscodeId: 'csharp',
     function: [
@@ -73,25 +63,6 @@ const CSharp: Language = {
             new QueryItem(tag.body, 'block'),
         ]),
     ],
-};
-const Java: Language = {
-    vscodeId: 'java',
-    function: [
-        new QueryItem(tag.fun, 'method_declaration', [
-            new QueryItem(tag.name, 'identifier'),
-            new QueryItem(tag.args, 'formal_parameters'),
-            new QueryItem(tag.body, 'block'),
-        ]),
-    ],
-};
-// ToDo: test (somehow)
-const Kotlin: Language = {
-    vscodeId: 'kotlin',
-    function: [],
-    // new Map([
-    //     ['function_declaration', 'simple_identifier'],
-    //     // 'anonymous_function'
-    // ]),
 };
 const Python: Language = {
     vscodeId: 'python',
@@ -109,7 +80,6 @@ import { JavaScript, TypeScript } from "./js";
 
 export const languages: Language[] = [
     C, Cpp, Rust, Go,
-    Ruby, Scala, Lua,
+    Java, CSharp, Python,
     JavaScript, TypeScript,
-    CSharp, Java, Kotlin, Python,
 ];
