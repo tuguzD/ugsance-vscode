0b0a6dffe6085d3fd23d16641eaed4bd70c17b03
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2025-02-28 13:03:30 +0300

    Reformat code a bit

--- src/tree-sitter/index.ts
+++ src/tree-sitter/index.ts
@@ -1,9 +1,8 @@
 import * as vscode from 'vscode';
 import * as files from 'fs';
-import Parser from 'web-tree-sitter';
 
 import { languages } from './languages';
-import { capturedQuery } from './queries';
+import * as query from './queries';
 import * as fun from './queries/function';
 
 export function register(context: vscode.ExtensionContext) {
@@ -21,53 +20,40 @@ async function useTreeSitter() {
         return;
     }
     const languageId = editor.document.languageId;
-
-    const config = vscode.workspace.getConfiguration('UGsance');
-    const userFolder = config.get<string>('tree-sitter.pathToWASM');
-    if (userFolder === "") {
-        const message = `You should set up folder for parsers (WASM files)!`;
-        vscode.window.showWarningMessage(message);
-        console.log(message);
-        return;
-    }
-
-    const path = `${userFolder}\\tree-sitter-${languageId}.wasm`;
-    if (!files.existsSync(path)) {
-        const message = `No parser for '${languageId}' located!`;
-        vscode.window.showErrorMessage(message);
-        console.log(message);
-        return;
-    }
-    const { parser, lang } = await initLanguage(path);
-    const tree = parser.parse(editor.document.getText());
-
     const langData = languages.find(item => item.vscodeId == languageId);
-    if (!langData || langData.function.length === 0) {
+    if (!langData) {
         const message = `The language '${languageId}' is not (currently) supported`;
         vscode.window.showErrorMessage(message);
         console.log(message);
         return;
     }
 
-    const functions = capturedQuery(
-        tree.rootNode, langData.function, lang, fun.tag.name,
+    const config = vscode.workspace.getConfiguration('UGsance');
+    const userFolder = config.get<string>('tree-sitter.pathToWASM');
+    if (!userFolder || userFolder === "") {
+        const message = `You should set up folder for parsers (WASM files)!`;
+        vscode.window.showWarningMessage(message);
+        console.log(message);
+        return;
+    }
+    const parserPath = `${userFolder}\\tree-sitter-${languageId}.wasm`;
+    if (!files.existsSync(parserPath)) {
+        const message = `No parser for '${languageId}' located!`;
+        vscode.window.showErrorMessage(message);
+        console.log(message);
+        return;
+    }
+
+    const { lang, node } = await query.initLanguage(
+        parserPath, editor.document.getText(),
     );
-    // for (let item of functions) console.log(
+    const captures = query.captures(node, langData.function, lang);
+    const functions = query.filterTag(captures, fun.tag.name);
+    // functions.forEach(item => console.log(
     //     `${item.node.startPosition.row}:${item.node.startPosition.column}`
-    // );
+    // ));
 
     const functionsNames = functions.map(capture => capture.node.text);
     vscode.window.showInformationMessage(functionsNames.toString());
     console.log(functionsNames);
-}
-
-async function initLanguage(path: string) {
-    await Parser.init();
-    const parser = new Parser();
-    const lang = await Parser.Language.load(path);
-    parser.setLanguage(lang);
-
-    // const queryText = files.readFileSync('', "utf-8");
-    // const highlightQuery = lang.query(queryText);
-    return { parser, lang };
 }
--- src/tree-sitter/queries/index.ts
+++ src/tree-sitter/queries/index.ts
@@ -1,5 +1,17 @@
 import Parser from "web-tree-sitter";
 
+export async function initLanguage(
+    parserPath: string, codeSource: string,
+) {
+    await Parser.init();
+    const parser = new Parser();
+    const lang = await Parser.Language.load(parserPath);
+    parser.setLanguage(lang);
+
+    const node = parser.parse(codeSource).rootNode;
+    return { lang, node };
+}
+
 export class QueryItem {
     tag: string | null;
     type: string;
@@ -23,13 +35,16 @@ export class QueryItem {
     }
 };
 
-export function capturedQuery(
-    node: Parser.SyntaxNode, queryType: QueryItem[],
-    parser: Parser.Language, captureTag: string | null = null,
+export function captures(
+    node: Parser.SyntaxNode, queryType: QueryItem[], parser: Parser.Language,
 ): Parser.QueryCapture[] {
     let query = queryType.map(item => item.query).join('\n\n');
-    let captures = parser.query(query).captures(node);
+    return parser.query(query).captures(node);
+}
 
-    return (captureTag == null) ? captures
-        : captures.filter(item => item.name === captureTag);
+export function filterTag(
+    captures: Parser.QueryCapture[], tag: string | null = null,
+) {
+    return (tag == null) ? captures
+        : captures.filter(item => item.name === tag);
 }
