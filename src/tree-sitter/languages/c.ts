2f5de24b1a5103e08ec03c939097c1a1b55297d9
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2025-03-01 15:21:22 +0300

    Update C/C++ "unit call" queries (finally and for real)

--- src/tree-sitter/languages/c.ts
+++ src/tree-sitter/languages/c.ts
@@ -1,27 +1,40 @@
 import { Language } from ".";
-import { QueryItem } from "../queries";
+import { Alternation, QueryItem } from "../queries";
 import { tag } from "../queries/function";
 
 export const C: Language = {
     vscodeId: 'c',
     callUnit: [
-        cQueryItem(false),
+        cQueryItem(['identifier']),
     ],
 };
 export const Cpp: Language = {
     vscodeId: 'cpp',
     callUnit: [
-        cQueryItem(true),
-        cQueryItem(false),
+        cQueryItem([
+            'identifier', 'field_identifier',
+            'qualified_identifier', 'operator_name',
+            'destructor_name', 'structured_binding_declarator',
+        ]),
+        new QueryItem(tag.call, 'lambda_expression', [
+            new QueryItem(tag.name, 'lambda_capture_specifier'),
+            new QueryItem(null, 'abstract_function_declarator', [
+                new QueryItem(tag.args, 'parameter_list'),
+            ], true),
+            new QueryItem(tag.body, 'compound_statement'),
+        ]),
     ],
 };
 
-function cQueryItem(method: boolean) {
-    let identifier = (method ? 'field_' : '') + 'identifier';
+function cQueryItem(nameTypes: string[]) {
+    let nameItem = new Alternation(null, nameTypes.map(
+        type => new QueryItem(tag.name, type),
+    ), true);
+
     return new QueryItem(
         tag.call, 'function_definition', [
-        new QueryItem(tag.body, 'function_declarator', [
-            new QueryItem(tag.name, identifier),
+        new QueryItem(null, 'function_declarator', [
+            nameItem,
             new QueryItem(tag.args, 'parameter_list'),
         ]),
         new QueryItem(tag.body, 'compound_statement'),
